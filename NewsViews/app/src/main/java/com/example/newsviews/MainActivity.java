33667cb172424fbd35b106b9dc3670a9cf92e2fe
Mahadi <ab.mahadi.h@gmail.com>
2020-02-26 07:32:59 +0600

    navigation drawer done

--- NewsViews/app/src/main/java/com/example/newsviews/MainActivity.java
+++ NewsViews/app/src/main/java/com/example/newsviews/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.newsviews;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar =findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        init();
+        ActionBarDrawerToggle toggle =new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+        super.onBackPressed();
+        }
+    }
+
+    private void init() {
+        drawerLayout =findViewById(R.id.drawe_layout_main_activity);
     }
 }
