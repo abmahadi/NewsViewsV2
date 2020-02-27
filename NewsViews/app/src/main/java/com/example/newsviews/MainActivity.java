b3ee186ed494b46f1f4c3a07fc20929142fb2d17
Mahadi <ab.mahadi.h@gmail.com>
2020-02-27 07:42:01 +0600

    Navigation Drawer Item Click

--- NewsViews/app/src/main/java/com/example/newsviews/MainActivity.java
+++ NewsViews/app/src/main/java/com/example/newsviews/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.newsviews;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.Switch;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         ActionBarDrawerToggle toggle =new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -38,5 +49,49 @@ public class MainActivity extends AppCompatActivity {
 
     private void init() {
         drawerLayout =findViewById(R.id.drawe_layout_main_activity);
+        navigationView = findViewById(R.id.nav_view);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId())
+        {
+            case R.id.nav_home :
+
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
+                break;
+            case R.id.nav_about :
+
+                startActivity(new Intent(getApplicationContext(),AboutActivity.class));
+
+                break;
+
+            case R.id.nav_exit :
+
+                //Exit pop up
+
+                new AlertDialog.Builder(this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Exit App")
+                        .setMessage("Are you sure you want to close this Application?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                android.os.Process.killProcess(android.os.Process.myPid());
+                                System.exit(1);
+                            }
+
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
+
+                break;
+
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
